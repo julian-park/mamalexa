e49ad0da1f93408be77c1b3ec6fb2e1b0400ef93
Li, Matthew <matli@paypal.com>
2016-06-25 15:07:44 -0700

    added sample recipe page, sample data

--- public/js/helloWorld.js
+++ public/js/helloWorld.js
@@ -1,11 +1,31 @@
 
 var HelloMessage = React.createClass({
   render: function() {
-    return <div>Hello {this.props.name}</div>;
+    return (
+    	<div>
+	    	<h1> INGREDIENTS </h1>
+	    	<ul>
+	    	{this.props.recipeData.ingredients.map((item)=>{
+	    		return (<li> {item} </li>)
+	    	})}
+	    	</ul> 
+	    </div>
+    );
   }
 });
 
+
+var recipeData = {
+	recipeName: 'cookie',
+	ingredients: [
+		'flour',
+		'sugar',
+		'somethin'
+	],
+	steps: ['do something', 'do something else', 'youre done']
+}
+
 ReactDOM.render(
-  <HelloMessage name="John" />,
-  document.body
+  <HelloMessage recipeData={recipeData} />,
+  document.getElementById('recipeInfo')
 );
