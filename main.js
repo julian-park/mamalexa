97e8094d327409664a355b8a5298ffa61b0e6fe1
lbkchen <lbkchen@gmail.com>
2016-06-25 18:22:31 -0700

    more stuff to main.js

--- main.js
+++ main.js
@@ -5,23 +5,25 @@ var cheerio = require("cheerio")
 var url = "http://allrecipes.com/search/results/?wt=chicken&sort=re"
 const baseurl = "http://allrecipes.com/search/results/"
 
-
-
-
-
-request(url, function(error, response, body) {
-    if (!error) {
-        var $ = cheerio.load(body);
-        var name = $("[class='grid-col__h3 grid-col__h3--recipe-grid']").text();
-        var body = $("[class='rec-card__description']").text();
-        var img = $()
-        name = stripInputToArray(name);
-        console.log(name);
-    }
-});
+function x() {
+    request(url, function(error, response, body) {
+        if (!error) {
+            var $ = cheerio.load(body);
+            var name = $("[class='grid-col__h3 grid-col__h3--recipe-grid']").text();
+            var body = $("[class='rec-card__description']").text();
+            var img = $("[class='grid-col__rec-image']").attr("data-original-src");
+            var imgs = $("[class='grid-col__rec-image']").get()
+            name = stripInputToArray(name);
+            console.log(name);
+            console.log(body);
+            console.log(img);
+            console.log(imgs[0]);
+        }
+    });
+}
 
 function stripInputToArray(str) {
-    return str.replace(/\s\s+/g, ",").split(",");
+    return str.replace(/\s\s+/g, "$").split("$");
 }
 
 /**
@@ -29,12 +31,36 @@ function stripInputToArray(str) {
     be used to access picture, title, or description.
 */
 function getParsedRecipesFromGrids(searchQuery) {
+    request(url, function(error, response, body) {
+        if (error) {
+            // handle error
+            console.error("Error in getting recipes from grids: " + error);
+            return
+        }
+        var $ = cheerio.load(body);
 
+        // get all recipe names
+        var name = $("[class='grid-col__h3 grid-col__h3--recipe-grid']").text();
+        name = stripInputToArray(name);
+
+        // get all recipe descriptions
+        // var body = $("[class='rec-card__description']").text();
+        // console.log(body);
+        var body = $("[class='rec-card__description']").get();
+        console.log(body);
+
+        // get all recipe image links
+        var imgs = $("[class='grid-col__rec-image']").get().map(function(obj) {return obj["attribs"]["data-original-src"]});
+        console.log(imgs);
+
+    });
 }
 
-function getURL(searchQuery, sortType = "re") {
-    return baseurl + "?wt=" + searchQuery + "sort=" + sortType;
-}
+getParsedRecipesFromGrids();
+
+// function getURL(searchQuery, sortType="re") {
+//     return baseurl + "?wt=" + searchQuery + "sort=" + sortType;
+// }
 // picture
 // title
 // description
