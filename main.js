5e0faec84391ab88185df348c199b92e79ab3a47
lbkchen <lbkchen@gmail.com>
2016-06-25 20:23:57 -0700

    rip mamalexa

--- main.js
+++ main.js
@@ -26,11 +26,25 @@ function stripInputToArray(str) {
     return str.replace(/\s\s+/g, "$").split("$");
 }
 
+function cleanArray(arr) {
+    var result = [];
+
+    for (var i = 0; i < arr.length; i++) {
+        thisVal = arr[i];
+        if (thisVal == "" || thisVal = null || thisVal == undefined) {
+            continue
+        }
+
+        if (thisVal.match(""))
+    }
+}
+
 /**
     Returns a list of objects, each with fields that can
     be used to access picture, title, or description.
 */
 function getParsedRecipesFromGrids(searchQuery) {
+    var numReturned = 6;
     request(url, function(error, response, body) {
         if (error) {
             // handle error
@@ -41,18 +55,20 @@ function getParsedRecipesFromGrids(searchQuery) {
 
         // get all recipe names
         var name = $("[class='grid-col__h3 grid-col__h3--recipe-grid']").text();
-        name = stripInputToArray(name);
+        name = stripInputToArray(name).slice(0, numReturned);
+        console.log(name);
 
         // get all recipe descriptions
         // var body = $("[class='rec-card__description']").text();
         // console.log(body);
-        var body = $("[class='rec-card__description']").get();
+        var body = $("[class='rec-card__description']").get().map(function(obj) {return obj["children"][0]["data"]}).slice(0, numReturned);
         console.log(body);
 
         // get all recipe image links
-        var imgs = $("[class='grid-col__rec-image']").get().map(function(obj) {return obj["attribs"]["data-original-src"]});
+        var imgs = $("[class='grid-col__rec-image']").get().map(function(obj) {return obj["attribs"]["data-original-src"]}).slice(0, numReturned);
         console.log(imgs);
 
+        console.log(name.length + " " + body.length + " " + imgs.length);
     });
 }
 
